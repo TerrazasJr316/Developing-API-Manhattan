ba2c19de5720f58e40148b00aafb3057a4009fbb
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-06-14 15:35:14 -0600

    update repo | 14 - Jun - 2025

--- app/manhattan.py
+++
@@ -1,8 +0,0 @@
-def manhattan_path(origin, destination):
-    # Ruta simple en L (horizontal primero, luego vertical)
-    path = [
-        [origin[0], origin[1]],
-        [destination[0], origin[1]],
-        [destination[0], destination[1]],
-    ]
-    return path
